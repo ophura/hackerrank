176889fae668494ec8508ea81a16b8344c0ba9a2
Ophura <61603742+ophura@users.noreply.github.com>
2023-08-31 00:30:49 +0200

    Update Program.cs

--- compare-the-triplets/Program.cs
+++ compare-the-triplets/Program.cs
@@ -1,7 +1,7 @@
-(int, int) Rate(IReadOnlyList<int> x, IReadOnlyList<int> y)
+(int, int) Rate(int[] x, int[] y)
 {
     (int z, int w) rating = default;
-    for (var i = 0; i < x.Count; i++)
+    for (var i = 0; i < x.Length; i++)
         if(x[i] > y[i]) rating.z++;
         else if (y[i] > x[i]) rating.w++;
     return (rating.z, rating.w);
