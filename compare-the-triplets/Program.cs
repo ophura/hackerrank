0ea533841619bec89aff021eddd2e72ca2564504
Ophura <61603742+ophura@users.noreply.github.com>
2023-08-31 00:27:30 +0200

    Update Program.cs

--- compare-the-triplets/Program.cs
+++ compare-the-triplets/Program.cs
@@ -1,16 +1,8 @@
-static List<int> Rate(IReadOnlyList<int> bobScores, IReadOnlyList<int> aliceScores)
+static (int, int) Rate(IReadOnlyList<int> x, IReadOnlyList<int> y)
 {
-    (int alice, int bob) rating = default;
-
-    var length = Math.Min(bobScores.Count, aliceScores.Count);
-
-    for (var i = 0; i < length; i++)
-    {
-        if (aliceScores[i] > bobScores[i]) rating.alice++;
-        else if (bobScores[i] > aliceScores[i]) rating.bob++;
-    }
-    
-    return flip(rating.alice, rating.bob);
-
-    static List<int> flip(int x, int y) => new(2) { y, x };
+    (int z, int w) rating = default;
+    for (var i = 0; i < x.Count; i++)
+        if(x[i] > y[i]) rating.z++;
+        else if (y[i] > x[i]) rating.w++;
+    return (rating.z, rating.w);
 }
