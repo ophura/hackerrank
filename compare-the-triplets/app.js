faea7d600a4ea3b9ba7139d37ed02e207ba18e58
Ophura <61603742+ophura@users.noreply.github.com>
2023-08-31 05:41:21 +0200

    Update app.js

--- compare-the-triplets/app.js
+++ compare-the-triplets/app.js
@@ -1,6 +1,6 @@
 const rate = (x, y) => {
     let z = 0, w = 0;
-    for (var i = 0; i < x.length; i++)
+    for (let i = 0; i < x.length; i++)
         if (x[i] > y[i]) z++;
         else if (y[i] > x[i]) w++;
     return [z, w];
